715447d530b33b952c35109b355dbd6a9099addf
Haisam Amin <h.amin@keepthinking.it>
2021-09-30 20:09:21 +0100

    Robot-name: Refactored code, based on mentor feedback, now using a Static variable, instead of mock persistance class, as it achieves the same

--- php/008_robot_name/robot-name.php
+++ php/008_robot_name/robot-name.php
@@ -10,35 +10,9 @@
 class Robot 
 {
 	private $name = NULL;
-	private $used_names;
-	protected $persistentStorage;
+	private static $used_names = [];
 
-	public function __construct(Persistance $persistentStorage = NULL)
-	{
-		$this->persistentStorage = $persistentStorage;
-		
-		// Exercism test doesn't provide depencency, so manually setting it up
-		if (!$this->persistentStorage)
-		{
-			$this->persistentStorage = new mockPersistantMemory();
-		}
-
-		$used_names = $this->persistentStorage->get('used_names');
-		if (!$used_names)
-		{
-			$used_names = [];
-			$this->persistentStorage->set('used_names', $used_names);
-		}
-
-		$this->used_names = $used_names;
-	}
-
-	public function __destruct()
-	{
-		$this->persistentStorage->set('used_names', $this->used_names);
-	}
-
-	public function getName($aaa = 0) : string
+	public function getName() : string
 	{
 		$this->name = $this->name ?? $this->generateName();
 		return $this->name;
@@ -57,60 +31,11 @@ class Robot
 			$ran_num = random_int(100, 999);
 
 			$rand_name = $ran_string . $ran_num;
-		} while (in_array($rand_name, $this->used_names));
+		} while (in_array($rand_name, $this::$used_names));
 		
-		$this->used_names[] = $rand_name;
+		$this::$used_names[] = $rand_name;
 
 		return $rand_name;
-	}
-}
-
-/**
- * An interface to be implemented by any storage dependency
- *
- * @package		Exercism PHP track
- * @author		hey-sam
- *
- */
-interface Persistance 
-{
-	public function setPersistance(&$persistance);
-	public function set($key, $data = NULL);
-	public function get($key);
-}
-
-/**
- * A mock persistant memory, that uses a superglobal, for the purpose of the Exercism test
- *
- * @package		Exercism PHP track
- * @author		hey-sam
- *
- */
-class mockPersistantMemory implements Persistance
-{
-	protected $memory;
-
-	public function __construct($persistance = NULL)
-	{
-		$persistance = !$persistance 
-			? $GLOBALS
-			: $persistance; 
-		$this->setPersistance($persistance);
-	}
-
-	public function setPersistance(&$persistance)
-	{
-		$this->memory = $persistance;
-	}
-
-	public function set($key, $data = NULL)
-	{
-		$this->memory[$key] = $data;
-	}
-
-	public function get($key)
-	{
-		return !empty($this->memory[$key]) ? $this->memory[$key] : NULL;
 	}
 }
 
